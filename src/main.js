a5af97299e6a1cf21368f801e08a6a5e83ea9add
Mesi Rendon <amrendonsa@unal.edu.co>
2019-08-24 12:23:07 -0500

    Starting dapp

--- src/main.js
+++ src/main.js
@@ -1,10 +1,22 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import Web3 from 'web3';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+window.addEventListener('load', () => {
+  if (web3 !== undefined) {
+    web3 = new Web3(window.web3.currentProvider);
+    Vue.prototype.$web3 = web3;
+    console.log('Detectamos metamask')
+  }
+  else {
+    console.warn('No hay metamask');
+  }
+});
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="alert alert-danger" role="alert" v-if="!provider">
+      No tienes un navegador compatible con la cadena de bloques. Te recomendamos <a
+            href="http://metamask.io" target="_blank">MetaMask</a>
+    </div>
+    <div class="alert alert-warning" role="alert" v-else>
+      Vemos que tienes MetaMask instalado. Por favor inicia sesión.
+    </div>
+    <div id="owner">
+      <h1>Bienvenido {ownerAddress}</h1>
+      <h2>Saldo</h2>
+      <h3>{Saldo}</h3>
+      <button class="btn btn-success btn-block">
+        Transferir fondos a mi cuenta
+      </button>
+    </div>
+    <div id="contributors">
+      <h1>Bienvenido contribuyente {contributorAddress}</h1>
+      <p>Por favor contribuye a mi causa:</p>
+      <form>
+        <div class="form-group">
+          <label for="amount">Fondos a transferir (ETH)</label>
+          <input type="number" class="form-control" id="amount" placeholder="Ex. 0.5" min="0">
+        </div>
+        <button type="submit" class="btn btn-primary btn-block">Enviar</button>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  export default {
+    name: 'home',
+    computed: {
+      provider() {
+        return this.$web3 !== undefined;
+      }
+    }
   }
-}
 </script>
